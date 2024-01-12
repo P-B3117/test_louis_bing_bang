e24ba924c72dcf3700cc5b48b5c0020a0756eb73
Louis Boisvert <louwoaf@gmail.com>
2024-01-11 19:44:37 -0500

    LouisTestPOT

--- src/mainlouis.cpp
+++ src/mainlouis.cpp
@@ -5,25 +5,40 @@
 
 #define BUTTON_PIN_1 4    
 #define BUTTON_PIN_2 5
+#define PWM_PIN_1 6
+#define PWM_PIN_2 7
+#define POT_PIN_1 11
+
+#define Val_Max_POT 4095
+#define Val_Min_POT 0
+#define New_Max_POT 180
+#define New_Min_POT 0
 
 Servo feed;
 Servo launch;
+
+int POT_Val = 0;       //est la valeurs du potentiomètre
 
 void setup () {
     Serial.begin(115200);
     pinMode(BUTTON_PIN_1, INPUT);
     pinMode(BUTTON_PIN_2, INPUT);
-    feed.attach(6,1000,2000);
-    launch.attach(7,1000,2000);
+    feed.attach(PWM_PIN_1,1000,2000);
+    launch.attach(PWM_PIN_2,1000,2000);
+    
 }
 
 void loop () {
-    //delay(75);
+    POT_Val = analogRead(POT_PIN_1);
+    POT_Val = map(POT_Val , Val_Min_POT, Val_Max_POT, New_Min_POT, New_Max_POT);     //va convertir la valeur du potentiomètre en valeurs numériques
+    Serial.println(POT_Val );    //devrait lire la valeur du potentiomètre
+
     if(digitalRead(BUTTON_PIN_2)== LOW)   //lis si le bouton est appuyer
     {
-        Serial.println ("vroom, vroom");    //envoie vroom vroom
+        Serial.println ("Avance roue avant");    //envoie vroom vroom
         launch.writeMicroseconds(1000);
         
+        launch.write(POT_Val );      //change la vitesse
     }
     else 
     {
@@ -33,9 +48,10 @@ void loop () {
 
     if(digitalRead(BUTTON_PIN_1)== LOW)   //lis si le bouton est appuyer
     {
-        Serial.println ("vroom, vroom");    //envoie vroom vroom
+        Serial.println ("Avance roue arrière");    //envoie vroom vroom
         feed.writeMicroseconds(1000);
         
+        launch.write(POT_Val );      //change la vitesse
     }
     else 
     {
@@ -43,8 +59,5 @@ void loop () {
         feed.writeMicroseconds(1500);
     }
 
-    //configuration PWM
-    
-//    Serial.println (DUTY_CYCLE); //envoie vroom
 
 }
